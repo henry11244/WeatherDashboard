2b641d884aff48b13b53260b38fe1d26942545f0
Henry Nguyen <hln11244@gmail.com>
2022-07-06 16:06:50 -0700

    added js comments

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,5 @@
+
+// Declaring variables
 var span = $('<span>')
 var superscript = $('<sup>')
 var degreeSign = superscript.text('o')
@@ -8,30 +10,40 @@ var windArray = []
 var humidityArray = []
 var uvArray = []
 var iconArray = []
+var cityInputValue = document.querySelector('#cityInputValue')
+
+// To pull dates
 var today = Date()
 var UniTime = parseInt(Math.floor(moment()))
 var miliSecondsInDay = 86400000
 var todays = new Date(UniTime + miliSecondsInDay * 3)
-console.log(todays)
 
+// To update the time every second
 setInterval(function () {
     timeBlock.text(moment()), 1000
 })
 
-
+// To pull from local storage the search history
 var locationSearches = []
 if (JSON.parse(localStorage.getItem('locationSearches') !== null)) { locationSearches = JSON.parse(localStorage.getItem('locationSearches')) }
 
-for (var i = 0; i < locationSearches.length; i++) {
-    var button = $('<button>')
-    button.click(function () {
-        cityInputValue.value = this.className
-        $('#cityInput').submit()
-    })
-    button.text(locationSearches[i]).addClass(locationSearches[i])
-    $('#priorSearches').append(button)
+// Populates the search history in the form of buttons on page load and after each search
+buttonFunction = () => {
+    for (var i = 0; i < locationSearches.length; i++) {
+        var button = $('<button>')
+        button.click(function () {
+            cityInputValue.value = this.className
+            $('#cityInput').submit()
+        })
+        button.text(locationSearches[i]).addClass(locationSearches[i])
+        $('#priorSearches').append(button)
+    }
 }
 
+// calling button function above on page load
+buttonFunction()
+
+// Adding 6 sections that will be used for the 5 day forecast plus current day
 for (var x = 0; x < 6; x++) {
     var section = $('<section>');
     section.addClass('dailyForecast')
@@ -49,18 +61,18 @@ for (var x = 0; x < 6; x++) {
     }
 }
 
+// creating invalid city message
 function newP() {
     var newP = $('<p>');
     newP.text('Enter valid city name').addClass('invalidCity');
     $('form').append(newP);
     $('.invalidCity').css('display', 'none');
-
 }
 
+// calling invalid city function above
 newP()
 
-var cityInputValue = document.querySelector('#cityInputValue')
-
+// funciton that is ran when form is submitted
 function submit(e) {
     e.preventDefault();
     $('#priorSearchesHeader').css('display', 'block');
@@ -78,8 +90,10 @@ function submit(e) {
     $('button').remove()
 }
 
+// calls submit function above when user submits the form
 $('#cityInput').submit(submit)
 
+// request to pull latitude and longitude based on city search
 var pullRequest = (cityName) => {
     $.ajax({
         url: `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=676c57c39d556b42d524ee448e04b39d`,
@@ -94,6 +108,7 @@ var pullRequest = (cityName) => {
         console.log(lon)
         success = true
 
+        // plugs lat/long from above into weather API to pull weather data 
         $.ajax({
             url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=676c57c39d556b42d524ee448e04b39d`,
             method: 'GET',
@@ -112,7 +127,7 @@ var pullRequest = (cityName) => {
             uvArray.push(currentUV)
             iconArray.push(currentIcon)
 
-
+            // populates arrays based on 5 day weather forecasts
             for (var i = 1; i < 6; i++) {
                 var wind = weatherData.daily[i].wind_speed;
                 var humidity = weatherData.daily[i].humidity;
@@ -126,10 +141,9 @@ var pullRequest = (cityName) => {
                 iconArray.push(icon)
             }
 
+            // adds weather data/icon to HTML webpage
             for (var i = 0; i < 6; i++) {
-
                 var imgs = document.querySelectorAll('img')
-
                 imgs[i].setAttribute('src', `https://openweathermap.org/img/wn/${iconArray[i]}@2x.png`)
                 imgs[i].style.display = ('block')
                 $(`.${i}Data`).children().eq(0).text(new Date(UniTime + miliSecondsInDay * i).toLocaleDateString('en-us', { weekday: "long", year: "numeric", month: "short", day: "numeric" })).addClass('text');
@@ -153,8 +167,9 @@ var pullRequest = (cityName) => {
                     $(`.${i}Data`).children().eq(5).css('background-color', 'green').css('height', '25px')
                 }
             }
-
         });
+
+        // if AJAX above is successful and the city is not already saved in local storage, add it to local storage 
     }).done(function () {
         repeat = false
         for (var i = 0; i < locationSearches.length; i++) {
@@ -164,17 +179,9 @@ var pullRequest = (cityName) => {
             locationSearches.unshift(cityName);
             localStorage.setItem('locationSearches', JSON.stringify(locationSearches))
         }
-        for (var i = 0; i < locationSearches.length; i++) {
-            var button = $('<button>')
-            button.click(function () {
-                cityInputValue.value = this.className
-                $('#cityInput').submit()
-            })
-            button.text(locationSearches[i]).addClass(locationSearches[i])
-            $('#priorSearches').append(button)
-        }
-
+        buttonFunction()
     })
+        // if AJAx fails, display fail message 
         .fail(function () {
             $('.invalidCity').css('display', 'block').css('color', 'red');
             $('#priorSearchesHeader').css('display', 'none');
